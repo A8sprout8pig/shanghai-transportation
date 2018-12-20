c9d7ced0ec788b486d008ef5e1c8d6755e0d8187
Cunjun Wang <13621691063@163.com>
2018-12-20 23:01:06 +0800

    添加DB和Mybatis配置，添加建表sql脚本，添加数据持久化接口

---
+++ db_script/release_ddl.sql
@@ -0,0 +1,25 @@
+create table if not exists bus_line
+(
+	id bigint auto_increment comment '主键',
+	bus_line varchar(200) not null comment '公交线路名',
+	up_going_start_station varchar(200) null comment '上行方向起点站',
+	up_going_terminal_station varchar(200) null comment '上行方向终点站',
+	up_going_first_time varchar(200) null comment '上行方向首班车时间',
+	up_going_last_time varchar(200) null comment '上行方向末班车时间',
+	up_going_station_count int null comment '上行方向站点数',
+	down_going_start_station varchar(200) null comment '下行方向起点站',
+	down_going_terminal_station varchar(200) null comment '下行方向终点站',
+	down_going_first_time varchar(200) null comment '下行方向首班车时间',
+	down_going_last_time varchar(200) null comment '下行方向末班车时间',
+	down_going_station_count int null comment '下行方向站点数',
+	line_length decimal(14,2) null comment '全场, 单位km',
+	bus_description varchar(200) null comment '线路描述',
+	bus_cost varchar(200) null comment '收费',
+	bus_comment varchar(200) null comment '备注',
+	is_del int null comment '逻辑删除标志符 0-未删除 1-已删除',
+	create_time datetime default current_timestamp null comment '创建时间',
+	update_time datetime default current_timestamp null comment '更新时间',
+	constraint bus_line_pk
+		primary key (id)
+)
+comment '公交线路信息';
---
+++ src/main/java/com/cunjunwang/shanghai/bus/query/controller/DataPersistenceController.java
@@ -0,0 +1,16 @@
+package com.cunjunwang.shanghai.bus.query.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by CunjunWang on 2018-12-20.
+ */
+@RestController
+@RequestMapping("/database")
+@Api(value = "数据持久化API")
+public class DataPersistenceController {
+
+
+}
